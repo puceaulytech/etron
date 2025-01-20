f814c6b77cf520fd191a3504b519ef8e36ca9ab2
Logan Lucas <logan.lucas83@gmail.com>
2025-01-20 16:09:04 +0100

    feat: add keys for player 2 to move

--- services/files/front/js/gamelogic.js
+++ services/files/front/js/gamelogic.js
@@ -22,9 +22,11 @@ playerTwo.placeInBoard(board);
 drawBoard(ctx, board);
 
 let playerOneMove = null;
+let playerTwoMove = null;
 
 document.addEventListener("keydown", (event) => {
     switch (event.key.toLowerCase()) {
+        /* Player 1 keys */
         case "q":
             playerOneMove = "LEFT";
             break;
@@ -43,14 +45,32 @@ document.addEventListener("keydown", (event) => {
         case "e":
             playerOneMove = "TOP_RIGHT";
             break;
-        default:
-            return;
+        /* Player 2 keys */
+        case "k":
+            playerTwoMove = "LEFT";
+            break;
+        case "m":
+            playerTwoMove = "RIGHT";
+            break;
+        case ";":
+            playerTwoMove = "BOTTOM_LEFT";
+            break;
+        case ":":
+            playerTwoMove = "BOTTOM_RIGHT";
+            break;
+        case "o":
+            playerTwoMove = "TOP_LEFT";
+            break;
+        case "p":
+            playerTwoMove = "TOP_RIGHT";
+            break;
     }
 });
 
 const intervalId = setInterval(() => {
     console.log("Playing turn...");
     if (playerOneMove) playerOne.move(board, playerOneMove);
+    if (playerTwoMove) playerTwo.move(board, playerTwoMove);
     drawBoard(ctx, board);
 
     if (gameDone(board)) {
@@ -59,4 +79,5 @@ const intervalId = setInterval(() => {
     }
 
     playerOneMove = null;
+    playerTwoMove = null;
 }, 500);
