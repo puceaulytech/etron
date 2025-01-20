b84edb635c1fda34d95862c6973ba1f31a5522d2
Romain Chardiny <romain.chardiny@gmail.com>
2025-01-20 16:32:43 +0100

    feat(gamesvc): add AI skeleton

---
+++ services/gamesvc/ai.js
@@ -0,0 +1,13 @@
+let gameState;
+
+function setup(playersState) {}
+
+function nextMove(playersState) {}
+
+function nextMoveStateless(gameState) {}
+
+exports = {
+    setup,
+    nextMove,
+    stateless: { nextMove: nextMoveStateless },
+};
