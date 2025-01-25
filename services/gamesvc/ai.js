ba1c6f5e8ac93ab2d9fb016eb292f6740ee1b1ef
Romain Chardiny <romain.chardiny@gmail.com>
2025-01-25 11:15:38 +0100

    wip: ai algorithm

--- services/gamesvc/ai.js
+++ services/gamesvc/ai.js
@@ -1,13 +1,92 @@
-let gameState;
+let globalGameState;
 
-function setup(playersState) {}
+function getGameState() {
+    return globalGameState;
+}
 
-function nextMove(playersState) {}
+const BOARD_HEIGHT = 9;
+const BOARD_WIDTH = 16;
+
+class GameState {
+    constructor(playerPosition, opponentPosition) {
+        this.playerPosition = playerPosition;
+        this.opponentPosition = opponentPosition;
+
+        this.board = new Array(BOARD_HEIGHT);
+
+        for (let i = 0; i < BOARD_HEIGHT; i++)
+            this.board[i] = new Array(
+                i % 2 === 0 ? BOARD_WIDTH : BOARD_WIDTH - 1,
+            ).fill(0);
+    }
+
+    updatePositions(playerPosition, opponentPosition) {
+        // Mark trails
+        this.board[this.playerPosition.row][this.playerPosition.column] =
+            "PLAYER";
+        this.board[this.opponentPosition.row][this.opponentPosition.column] =
+            "OPPONENT";
+
+        this.playerPosition = playerPosition;
+        this.opponentPosition = opponentPosition;
+    }
+}
+
+function switchPlayer(player) {
+    return "PLAYER" ? "OPPONENT" : "PLAYER";
+}
+
+function negamax(gameState, currentPlayer, depth, alpha, beta) {
+    if (depth <= 0 /* || isGameOver */) {
+        // return heuristic or big ass number if game over, negative if currentPlayer is opponent, positive otherwise
+    }
+
+    const legalMoves = []; /* = getLegalMoves(); */
+    let score = Number.NEGATIVE_INFINITY;
+
+    for (const legalMove of legalMoves) {
+        // apply move
+
+        score = Math.max(
+            score,
+            -negamax(
+                gameState,
+                depth - 1,
+                -beta,
+                -alpha,
+                switchPlayer(currentPlayer),
+            ),
+        );
+
+        // revert move
+
+        alpha = Math.max(alpha, score);
+        if (alpha >= beta) break;
+    }
+
+    return score;
+}
+
+async function setup(playersState) {
+    globalGameState = new GameState(
+        playersState.playerPosition,
+        playersState.opponentPosition,
+    );
+
+    return true;
+}
+
+async function nextMove(playersState) {
+    globalGameState.updatePositions(playersState);
+
+    return nextMoveStateless(globalGameState);
+}
 
 function nextMoveStateless(gameState) {}
 
-exports = {
+module.exports = {
     setup,
     nextMove,
+    getGameState,
     stateless: { nextMove: nextMoveStateless },
 };
