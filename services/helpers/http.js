836477088c115bf9b3bd4561b01ca3d9415eabc1
Romain Chardiny <romain.chardiny@gmail.com>
2025-02-03 13:47:22 +0100

    feat(helpers): add http helper to decode body

---
+++ services/helpers/http.js
@@ -0,0 +1,30 @@
+const http = require("http");
+
+/**
+ * Decodes the body of the request to JSON
+ *
+ * @param {http.ClientRequest} req The HTTP request
+ * @returns {Promise<any>} The JSON body
+ */
+function decodeJsonBody(req) {
+    return new Promise((resolve, reject) => {
+        let body = "";
+
+        req.on("data", (chunk) => {
+            body += chunk.toString();
+        });
+
+        req.on("end", () => {
+            try {
+                const jsonData = JSON.parse(body);
+                resolve(jsonData);
+            } catch (err) {
+                reject(new Error("Invalid JSON"));
+            }
+        });
+
+        req.on("error", (err) => {
+            reject(err);
+        });
+    });
+}
