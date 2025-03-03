e397a4241fd10a191628618be6b5ee9a6aec1753
Logan Lucas <logan.lucas83@gmail.com>
2025-03-03 16:41:45 +0100

    feat(friends): can accept friend requests in back

--- services/social/controllers.js
+++ services/social/controllers.js
@@ -12,6 +12,9 @@ const endpoints = {
         GET: getFriendRequests,
         POST: addFriend,
     },
+    friends: {
+        POST: acceptFriend,
+    },
 };
 
 /**
@@ -100,6 +103,68 @@ async function addFriend(req, res) {
     );
 
     return { ok: "bro" };
+}
+
+/**
+ * Accept a pending friend request
+ *
+ * @param {http.ClientRequest} req
+ * @param {http.ServerResponse} res
+ */
+async function acceptFriend(req, res) {
+    const userId = authenticate(req, res, jwt);
+    if (!userId) return;
+
+    const userCollection = pool.get().collection("users");
+
+    const user = await userCollection.findOne({
+        _id: ObjectId.createFromHexString(userId),
+    });
+    if (!user) {
+        sendError(
+            res,
+            401,
+            "E_USER_DOES_NOT_EXIST",
+            "User that generated token does not exist anymore.",
+        );
+        return;
+    }
+
+    const payload = await decodeJsonBody(req);
+    if (!payload.newFriendId) {
+        sendError(
+            res,
+            422,
+            "E_MISSING_BODY_FIELD",
+            "No user id provided to accept friend request ('newFriendId').",
+        );
+        return;
+    }
+
+    const { friendRequests } = user;
+    if (!friendRequests.includes(payload.newFriendId)) {
+        sendError(
+            res,
+            400,
+            "E_NOT_IN_FRIEND_REQUESTS",
+            "Tried to accept a non-existing friend request.",
+        );
+        return;
+    }
+
+    userCollection.updateOne(
+        { _id: ObjectId.createFromHexString(payload.newFriendId) },
+        { $push: { friends: userId } },
+    );
+    userCollection.updateOne(
+        { _id: ObjectId.createFromHexString(userId) },
+        {
+            $push: { friends: payload.newFriendId },
+            $pull: { friendRequests: payload.newFriendId },
+        },
+    );
+
+    return { still: "ok bro" };
 }
 
 module.exports = createHandler(endpoints, (res) => {
