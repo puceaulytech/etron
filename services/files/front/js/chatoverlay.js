f9832023cba69f717027588daf9769ae47d639b0
Logan Lucas <logan.lucas83@gmail.com>
2025-03-20 09:39:36 +0100

    feat(chat): display correct messages in chat
    they aren't updated in real time yet

--- services/files/front/js/chatoverlay.js
+++ services/files/front/js/chatoverlay.js
@@ -1,26 +1,42 @@
 const chatOverlay = document.querySelector("#chat-section");
 const backButton = document.querySelector("#chat-header #chat-back-button");
 const friendNameContainer = document.querySelector("#chat-header #friend-name");
+const messagesContainer = document.querySelector("#chat-section #chat-itself");
 /** @type {HTMLInputElement} */
 const messageInput = document.querySelector("#chat-input-container input");
 
 let lastFriendId;
 
 function scrollToBottom() {
-    const messagesContainer = document.querySelector(
-        "#chat-section #chat-itself",
-    );
     messagesContainer.scrollTop = messagesContainer.scrollHeight;
 }
 
-function openChat(friendName, friendId) {
+async function openChat(friendName, friendId) {
     if (friendId !== lastFriendId) {
         lastFriendId = friendId;
         friendNameContainer.innerHTML = friendName;
         messageInput.setAttribute("placeholder", `Message ${friendName}`);
         messageInput.value = "";
 
-        // TODO: real messages
+        await authenticatedFetch(`/api/social/chat/${friendId}`, {
+            method: "GET",
+        })
+            .then((messages) =>
+                messages.map((message) => {
+                    const div = document.createElement("div");
+                    div.innerText = message.content;
+                    div.classList.add(
+                        "chat-message",
+                        friendId === message.sender
+                            ? "received-message"
+                            : "sent-message",
+                    );
+                    return div;
+                }),
+            )
+            .then((divs) => {
+                messagesContainer.replaceChildren(...divs);
+            });
     }
 
     chatOverlay.classList.remove("invisible");
