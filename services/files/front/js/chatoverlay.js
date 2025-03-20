9c4d45aa18c883130259b688947762cbe6f82a6e
Logan Lucas <logan.lucas83@gmail.com>
2025-03-20 10:42:25 +0100

    feat(chat): send messages
    they still aren't updated in real time

--- services/files/front/js/chatoverlay.js
+++ services/files/front/js/chatoverlay.js
@@ -1,11 +1,28 @@
 const chatOverlay = document.querySelector("#chat-section");
 const backButton = document.querySelector("#chat-header #chat-back-button");
 const friendNameContainer = document.querySelector("#chat-header #friend-name");
+
 const messagesContainer = document.querySelector("#chat-section #chat-itself");
+
+const chatForm = document.querySelector("#chat-section #chat-input-container");
 /** @type {HTMLInputElement} */
-const messageInput = document.querySelector("#chat-input-container input");
+const messageInput = document.querySelector(
+    "#chat-input-container input[name='message']",
+);
 
 let lastFriendId;
+let lastSubmitHandler;
+
+async function sendChatTo(friendId, message) {
+    await authenticatedFetch("/api/social/chat", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            content: message,
+            receiver: friendId,
+        }),
+    });
+}
 
 function scrollToBottom() {
     messagesContainer.scrollTop = messagesContainer.scrollHeight;
@@ -13,10 +30,26 @@ function scrollToBottom() {
 
 async function openChat(friendName, friendId) {
     if (friendId !== lastFriendId) {
-        lastFriendId = friendId;
         friendNameContainer.innerHTML = friendName;
         messageInput.setAttribute("placeholder", `Message ${friendName}`);
         messageInput.value = "";
+
+        const submitHandler = async (event) => {
+            event.preventDefault();
+
+            const message = messageInput.value;
+            if (message === "") return;
+
+            await sendChatTo(friendId, message);
+            scrollToBottom();
+            messageInput.value = "";
+        };
+
+        if (lastSubmitHandler)
+            chatForm.removeEventListener("submit", lastSubmitHandler);
+        chatForm.addEventListener("submit", submitHandler);
+        lastSubmitHandler = submitHandler;
+        lastFriendId = friendId;
 
         await authenticatedFetch(`/api/social/chat/${friendId}`, {
             method: "GET",
