aedf26cfaf76b3d332e2978bcaae043c319070ec
Logan Lucas <logan.lucas83@gmail.com>
2025-03-26 12:48:26 +0100

    feat(online): display opponent name on top of game grid

--- services/files/front/components/dialog/dialog.js
+++ services/files/front/components/dialog/dialog.js
@@ -10,6 +10,7 @@ class AppDialog extends HTMLElement {
                     position: fixed;
                     top: 0;
                     left: 0;
+                    z-index: 1000;
                     width: 100%;
                     height: 100%;
                     background: rgba(0, 0, 0, 0.5);
