fdf46c6831ea5eb74eedec5ad70c6b94dc979a25
Logan Lucas <logan.lucas83@gmail.com>
2025-03-28 14:03:05 +0100

    feat(chat): animation when sending or receiving messages

--- services/files/front/js/chatoverlay.js
+++ services/files/front/js/chatoverlay.js
@@ -30,18 +30,22 @@ function scrollToBottom() {
     messagesContainer.scrollTop = messagesContainer.scrollHeight;
 }
 
-function createMessageDiv(content, received) {
+function createMessageDiv(content, received, animated) {
     const div = document.createElement("div");
     div.innerText = content;
     div.classList.add(
         "chat-message",
         received ? "received-message" : "sent-message",
     );
+    if (animated) {
+        div.classList.add("animated");
+        setTimeout(() => div.classList.remove("animated"), 160);
+    }
     return div;
 }
 
 function insertMessage(content, received) {
-    messagesContainer.appendChild(createMessageDiv(content, received));
+    messagesContainer.appendChild(createMessageDiv(content, received, true));
 }
 
 async function openChat(friendName, friendId) {
@@ -76,6 +80,7 @@ async function openChat(friendName, friendId) {
                     createMessageDiv(
                         message.content,
                         friendId === message.sender,
+                        false,
                     ),
                 ),
             )
