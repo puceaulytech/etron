91de7d1f707cc1694fe681122e463f0b4915846d
Logan Lucas <logan.lucas83@gmail.com>
2025-04-03 14:37:04 +0200

    fix(anetrotro): BOT is always online

--- services/gamesvc/startup.js
+++ services/gamesvc/startup.js
@@ -1,9 +1,14 @@
 const pool = require("../helpers/db");
 
+const BOT_USERNAME = process.env["BOT_USERNAME"];
+
 async function setEveryoneOffline() {
     const userCollection = pool.get().collection("users");
 
-    await userCollection.updateMany({}, { $set: { online: false } });
+    await userCollection.updateMany(
+        { username: { $ne: BOT_USERNAME } },
+        { $set: { online: false } },
+    );
 }
 
 module.exports = { setEveryoneOffline };
