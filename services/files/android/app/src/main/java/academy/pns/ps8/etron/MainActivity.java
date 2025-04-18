a3ab79e1f92a04b729b0186d182b256be1cd3efc
Romain Chardiny <romain.chardiny@gmail.com>
2025-04-18 13:16:10 +0200

    feat(mobile): make some stuff work

--- services/files/android/app/src/main/java/academy/pns/ps8/etron/MainActivity.java
+++ services/files/android/app/src/main/java/academy/pns/ps8/etron/MainActivity.java
@@ -1,5 +1,23 @@
 package academy.pns.ps8.etron;
 
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.View;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        View decorView = getWindow().getDecorView();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null)
+            actionBar.hide();
+    }
+}
