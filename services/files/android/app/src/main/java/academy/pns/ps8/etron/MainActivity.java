73686cb240566a108ef8bdd9aa6c26ba8d5dd328
Romain Chardiny <romain.chardiny@gmail.com>
2025-04-23 09:10:26 +0200

    fix(android): better system bar handling

--- services/files/android/app/src/main/java/academy/pns/ps8/etron/MainActivity.java
+++ services/files/android/app/src/main/java/academy/pns/ps8/etron/MainActivity.java
@@ -13,7 +13,13 @@ public class MainActivity extends BridgeActivity {
 
         View decorView = getWindow().getDecorView();
 
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        int uiOptions = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+
         decorView.setSystemUiVisibility(uiOptions);
 
         ActionBar actionBar = getActionBar();
